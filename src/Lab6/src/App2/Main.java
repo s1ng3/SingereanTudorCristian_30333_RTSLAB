7fcfc2c89c46d4c4976aef2ec857d7e1ca011e54
Tudor-Cristian Sîngerean <tudor.singerean@yahoo.com>
2024-05-24 14:43:42 +0300

    Adding Lab6

---
+++ src/Lab6/src/App2/Main.java
@@ -0,0 +1,29 @@
+package App2;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
+public class Main
+{
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch;
+        Semaphore sem1 = new Semaphore(1);
+        Semaphore sem2 = new Semaphore(1);
+
+        int[] times1 = {2, 4, 4};
+        int[] times2 = {2, 5, 5};
+        int[] times3 = {3, 6, 3};
+
+
+        while(true) {
+            latch = new CountDownLatch(4);
+            new ExecutionThread1(latch, sem1, times1).start();
+            new ExecutionThread1(latch, sem2, times2).start();
+            new ExecutionThread2(latch, sem1, sem2, times3).start();
+
+            latch.countDown();
+            latch.await();
+
+        }
+    }
+
+}
